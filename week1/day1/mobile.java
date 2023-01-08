b45723a438fb16ede1994b1a8bc4e12cf1692a5b
kamalanathan <87558395+Kamalanathan-Saravanan@users.noreply.github.com>
2023-01-08 14:01:14 +0530

    Add files via upload
    Week 1

---
+++ week1/day1/mobile.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class mobile {
+
+	public static void main(String[] args) {
+		int modelNo = 96756;
+		boolean isAndroid = true;
+		float mobilePrice = 12000.45f;
+		
+		System.out.println(modelNo);
+		System.out.println(isAndroid);
+		System.out.println(mobilePrice);
+	
+		// TODO Auto-generated method stub
+
+	}
+
+}
